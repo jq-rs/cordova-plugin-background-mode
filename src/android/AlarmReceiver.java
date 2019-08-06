f64b2c44f3828a90a60a361f55d720ee9d062f4e
jq-rs <jq-rs@mles.io>
2019-08-06 20:49:20 +0300

    Wifilock addition.

--- src/android/AlarmReceiver.java
+++ src/android/AlarmReceiver.java
@@ -2,6 +2,8 @@
 package de.appplant.cordova.plugin.background;
 
 import android.os.PowerManager;
+import android.net.wifi.WifiManager;
+import android.net.wifi.WifiManager.WifiLock;
 import android.content.Context;
 import android.content.Intent;
 import android.content.BroadcastReceiver;
@@ -15,6 +17,7 @@ import android.content.BroadcastReceiver;
 
 import static android.content.Context.POWER_SERVICE;
 import static android.os.PowerManager.PARTIAL_WAKE_LOCK;
+import static android.net.wifi.WifiManager.WIFI_MODE_FULL;
 
 public class AlarmReceiver extends BroadcastReceiver {
 	private PowerManager.WakeLock wakeLock;
@@ -24,13 +27,16 @@ public class AlarmReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.d("MlesAlarm", "Got alarm");
-		
         PowerManager pm = (PowerManager)context.getSystemService(POWER_SERVICE);
+		WifiManager wm = (WifiManager)context.getSystemService(Context.WIFI_SERVICE);
 
         wakeLock = pm.newWakeLock(
                 PARTIAL_WAKE_LOCK, "backgroundmode:wakelock");
 
         wakeLock.acquire();
+		
+		WifiLock wfl = wm.createWifiLock(WifiManager.WIFI_MODE_FULL, "backgroundmode:sync_all_wifi");
+		wfl.acquire();
 				
 		alarmMgr = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
 		Intent nextIntent = new Intent(context, AlarmReceiver.class);
@@ -40,8 +46,9 @@ public class AlarmReceiver extends BroadcastReceiver {
 		alarmMgr.setExactAndAllowWhileIdle(AlarmManager.ELAPSED_REALTIME_WAKEUP,
 										   SystemClock.elapsedRealtime() + 120 * 1000, alarmIntent);
 
+		wfl.release();
 		wakeLock.release();
         wakeLock = null;
-		Log.d("MlesAlarm", "Got and released wakelock");
+		Log.d("MlesAlarm", "Got and released wakelock and wifilock");
 	}
 }
